5445c1f401034a2aaa22f4001c743632cc1f6888
aceladines <ajladines01@gmail.com>
2024-06-01 06:05:30 +0800

    [REMOVE] - Sample usage

--- Program.cs
+++ Program.cs
@@ -3,13 +3,6 @@ internal class Program
 {
     private static void Main()
     {
-        SimpleLinkedList<int> linkedList = new();
 
-        linkedList.InsertAtBeggining(1);
-        linkedList.InsertAtBeggining(2);
-        linkedList.InsertAtBeggining(3);
-        linkedList.DeleteFromBeginning();
-
-        linkedList.PrintList();
     }
 }
